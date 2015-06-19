82eed5244515e8e09b61028d961649cd9efe1745
Anna Bedell <awbedell@gmail.com>
2015-06-19 15:14:50 -0400

    styling

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,8 @@
 class List < ActiveRecord::Base
   has_many :tasks
   belongs_to :user
-  accepts_nested_attributes_for :tasks, allow_destroy: true
+  accepts_nested_attributes_for :tasks, reject_if: proc { |attributes| attributes['name'].blank?},
+      allow_destroy: true
 
   def next_order_number
     self.tasks.count + 1
