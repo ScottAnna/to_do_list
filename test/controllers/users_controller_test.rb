c25c1cf764b511205d402adcf3b9a4c0e53029a7
Scott Williams <williamsjs1224@gmail.com>
2015-06-21 19:11:19 -0400

    add session[:user_id] to user controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
   setup do
     @user = users(:one)
+    session[:user_id] = @user.id
   end
 
   test "should get index" do
@@ -24,4 +25,26 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to user_path(assigns(:user))
   end
 
+  test "should show user" do
+    get :show, id: @user
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @user
+    assert_response :success
+  end
+
+  test "should update user" do
+    patch :update, id: @user, user: { email: @user.email, password_digest: @user.password_digest }
+    assert_redirected_to user_path(assigns(:user))
+  end
+
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete :destroy, id: @user
+    end
+
+    assert_redirected_to users_path
+  end
 end
